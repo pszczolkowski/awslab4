fbc0646aa1bc6ca29dec2a1b6dcc75c4bb82ba05
pszczolkowski <pszczolkowski.k@gmail.com>
2016-05-10 11:08:14 +0200

    costamwiecej

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,4 +1,5 @@
 var AWS = require("aws-sdk");
+var helpers = require("../helpers");
 
 AWS.config.loadFromPath('./config.json');
 var s3 = new AWS.S3();
@@ -11,8 +12,21 @@ var task =  function(request, callback){
 	};
 	
 	s3.getObject(params, function(err, data) {
-	  if (err) callback(err); // an error occurred
-	  else     callback(null, data);           // successful response
+	  if (err) callback(err);
+	  
+	  else     {
+		var algorithms = ['md5', 'sha1', 'sha256', 'sha512'];
+	var loopCount = 1;
+	
+	
+	helpers.calculateMultiDigest(data.Body.toString(), 
+		algorithms, 
+		function(err, digests) {
+			callback(null, JSON.stringify(data.Metadata) + "<br />" + digests.join("<br />")); 
+		}, 
+		loopCount);
+	  
+	  }
 	});
 	
 	
